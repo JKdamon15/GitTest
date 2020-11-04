087ee76a5be010dc02cdea8ce2631e5270e8d128
jack.miao <jack.miao@139.com>
2020-11-04 08:14:47 +0800

    修改commite文件，初始化类

--- src/test/showtime/Commited_white.java
+++ src/test/showtime/Commited_white.java
@@ -6,4 +6,26 @@ package test.showtime;
  * @date： 2020/11/4 6:45
  */
 public class Commited_white {
+    public String name;
+    private int id;
+
+    public Commited_white(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
